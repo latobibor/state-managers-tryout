c02b2799f702e136685bfb6f744cc07fbb766829
Tóth András <toth83andras@yahoo.com>
2020-03-23 17:30:11 +0100

    Unit test fix

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App without crashing', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Main starts/i);
   expect(linkElement).toBeInTheDocument();
 });
