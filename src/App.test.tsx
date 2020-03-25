c7ec12981094efb5eaf727ab9ff3af54f3b33343
Tóth András <toth83andras@yahoo.com>
2020-03-25 17:27:04 +0100

    unit test fixes

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,22 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders App without crashing', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Main starts/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App tests', () => {
+  beforeAll(() => {
+    window.matchMedia =
+      window.matchMedia ||
+      function () {
+        return {
+          matches: false,
+          addListener: jest.fn(),
+          removeListener: jest.fn(),
+        };
+      };
+  });
+
+  test('renders App without crashing', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/State Management Try Outs/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
