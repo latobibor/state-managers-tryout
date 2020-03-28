0f8991d91972e3a8396a19a2450f8bbc2c1178ac
Tóth András <toth83andras@yahoo.com>
2020-03-28 13:57:38 +0100

    sending messages with Enter works now, but it makes new line with Shift+Enter

--- src/components/chat-area/message-editor.tsx
+++ src/components/chat-area/message-editor.tsx
@@ -1,27 +1,36 @@
-import React from 'react';
+import React, { createRef, KeyboardEvent } from 'react';
 import { Input, Button, Form } from 'antd';
 import styles from './message-editor.module.less';
 import { useDispatch } from 'react-redux';
 import { Actions } from '../../redux/root-reducer';
+import { FormInstance } from 'antd/lib/form';
 
 const { TextArea } = Input;
 
 export function MessageEditor() {
+  const formReference = createRef<FormInstance>();
+
   const dispatch = useDispatch();
 
   // this sucks I need to find a way to load antd's own interface for this function
   function sendMessage(values: any) {
     const { message } = values;
-    console.log('ENTER', message);
 
     dispatch({ type: Actions.AddMessage, message });
+    formReference.current?.resetFields();
+  }
+
+  function handleEnter(e: KeyboardEvent<HTMLTextAreaElement>) {
+    if (!e.shiftKey) {
+      formReference.current?.submit();
+    }
   }
 
   return (
     <div className={styles['message-editor']}>
-      <Form name="message-editor" onFinish={sendMessage}>
+      <Form name="message-editor" ref={formReference} onFinish={sendMessage}>
         <Form.Item name="message">
-          <TextArea rows={5} />
+          <TextArea rows={5} onPressEnter={handleEnter} />
         </Form.Item>
         <Form.Item>
           <Button className={styles['send-button']} type="primary" htmlType="submit">
