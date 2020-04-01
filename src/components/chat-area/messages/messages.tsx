3b27c7a622c332252dac0467d1dc1bdae3b5da3b
Tóth András <toth83andras@yahoo.com>
2020-04-01 19:52:52 +0200

    fix order of messages

--- src/components/chat-area/messages/messages.tsx
+++ src/components/chat-area/messages/messages.tsx
@@ -14,12 +14,26 @@ function getCurrentUserId(globalState: GlobalState): string {
   return globalState.currentUser.id;
 }
 
+function sortMessagesByTime(a: MessageData, b: MessageData): number {
+  if (a.time > b.time) {
+    return 1;
+  }
+
+  if (a.time === b.time) {
+    return 0;
+  }
+
+  return -1;
+}
+
 export function Messages({ messages }: MessagesProps) {
   const currentUserId = useSelector<GlobalState, string>(getCurrentUserId);
 
+  const sortedMessages = messages.sort(sortMessagesByTime);
+
   return (
     <div className={styles.messages}>
-      {messages.map((message) => (
+      {sortedMessages.map((message) => (
         <Message key={message.time.toISOString()} text={message.body} isSent={currentUserId === message.from.id} />
       ))}
     </div>
