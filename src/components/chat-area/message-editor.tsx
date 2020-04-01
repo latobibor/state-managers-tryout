bab79459d14515297ce798d82e97f05bf6116085
Tóth András <toth83andras@yahoo.com>
2020-04-01 19:49:15 +0200

    sending message got fixed

--- src/components/chat-area/message-editor.tsx
+++ src/components/chat-area/message-editor.tsx
@@ -1,23 +1,43 @@
 import React, { createRef, KeyboardEvent } from 'react';
 import { Input, Button, Form } from 'antd';
 import styles from './message-editor.module.less';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Actions, DispatchAction, AddMessageAction } from '../../redux/root-reducer';
 import { FormInstance } from 'antd/lib/form';
+import { GlobalState } from '../../redux/global-state';
+import { User } from '../../clients/user-data';
 
 const { TextArea } = Input;
+
+type MessageEditorProps = {
+  activeChatId: string | undefined;
+  currentUser: User;
+};
 
 export function MessageEditor() {
   const formReference = createRef<FormInstance>();
 
   const dispatch = useDispatch<DispatchAction<AddMessageAction>>();
+  const { activeChatId, currentUser } = useSelector<GlobalState, MessageEditorProps>(({ activeChatId, currentUser}) => ({
+    activeChatId,
+    currentUser
+  }))
 
   // this sucks I need to find a way to load antd's own interface for this function
   function sendMessage(values: any) {
-    const { message } = values;
+    const { messageBody } = values;
 
-    dispatch({ type: Actions.AddMessage, message });
+    const message = {
+      // todo: the || '' should be unnecessary
+      chatId: activeChatId || '',
+      from: currentUser,
+      body: messageBody,
+      time: new Date(),
+      isRead: true,
+    }
+
     formReference.current?.resetFields();
+    dispatch({ type: Actions.AddMessage, message });
   }
 
   function handleEnter(e: KeyboardEvent<HTMLTextAreaElement>) {
@@ -26,10 +46,12 @@ export function MessageEditor() {
     }
   }
 
+  // todo: disable on missing activeChatId
+
   return (
     <div className={styles['message-editor']}>
       <Form name="message-editor" ref={formReference} onFinish={sendMessage}>
-        <Form.Item name="message" className={styles['form-item']}>
+        <Form.Item name="messageBody" className={styles['form-item']}>
           <TextArea rows={5} onPressEnter={handleEnter} />
         </Form.Item>
         <Form.Item className={styles['form-item']}>
