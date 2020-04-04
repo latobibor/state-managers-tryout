720f66c8913e95b3537b7fbf52c1118e924dde33
Tóth András <toth83andras@yahoo.com>
2020-04-05 00:05:23 +0200

    add some more messages just for giggles

--- src/clients/message-websocket.ts
+++ src/clients/message-websocket.ts
@@ -22,7 +22,8 @@ function resolveTemplate(): string {
 
   for (let i = 0; i < substitution; i++) {
     const wordIndex = Math.floor(Math.random() * words.length);
-    resultTemplate = resultTemplate.replace(`{${i}}`, words[wordIndex]);
+    const regex = new RegExp(`\\{${i}\\}`, 'g');
+    resultTemplate = resultTemplate.replace(regex, words[wordIndex]);
   }
 
   return resultTemplate;
@@ -62,6 +63,15 @@ const templates: TemplateMessage[] = [
     template: 'Having eaten 1 {0}, I concluded that {1} should not be {2}',
     substitution: 3,
   },
+  { template: 'But if {0} is true, that can only mean that {1} is also.', substitution: 2 },
+  { template: 'Either {0} is {1}, or it is not the case that {0} is {1}.', substitution: 2 },
+  { template: 'Courage is not {0} of the {1}', substitution: 2 },
+  {
+    template:
+      'It is impossible that the same {0} belong and not belong to the same {1} at the same {2} and in the same {3}.',
+    substitution: 4,
+  },
+  { template: 'If T is a {0} and F is {1} then {2} is also true', substitution: 3 },
 ];
 
 const words: string[] = [
@@ -70,7 +80,7 @@ const words: string[] = [
   'goat cheese',
   'stinky',
   'truth',
-  'magnificient',
+  'magnificent',
   'side',
   'duck',
   'liver',
